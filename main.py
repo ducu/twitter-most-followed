dca1f33017bf19145ff06daeedc629b142a4134b
Alexandru Stanciu <alexandru.stanciu@gmail.com>
2014-08-07 09:00:43 +0200

    Prepare abstract storage

--- main.py
+++ main.py
@@ -11,11 +11,30 @@ Top most followed by @newsyc20 followers.
 import twitter as t
 from twitter import TweepError
 
+import storage as s
+
 user_data = lambda u: dict([(k, getattr(u, k)) for k in \
 	['screen_name', 'name', 'description', 'friends_count', 'followers_count']])
 
 def main():
-	pass
+
+	# Step 1: Load @someones followers
+	source_name = 'newsyc20'
+	source = t.get_user(screen_name=source_name)
+	source_id = source.id
+	# source_id = 148969874
+	s.set_user_data(source_id, **user_data(source))
+
+	followers = t.followers_ids(source_id)
+	s.set_followers(source_id, *followers)
+	# followers = s.get_followers(source_id)
+
+
+	# Step 2: Load followers' friends
+
+
+	# Step 3: Aggregate most followed
+
 
 if __name__ == '__main__':
 	main()
--- storage.py
+++ storage.py
@@ -1,5 +1,9 @@
 
-import redis
+def set_user_data(user_id, **user_data):
+	print "\nSet user data: %s\n%s" % (user_id, user_data)
 
-r = redis.StrictRedis()
+def set_friends(user_id, *friends):
+	print "\nSet user friends: %s\n%s" % (user_id, friends)
 
+def set_followers(user_id, *followers):
+	print "\nSet user followers: %s\n%s" % (user_id, followers)
