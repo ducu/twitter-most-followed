8de29ea178d9c9a7067427d80d6cfb2e760805a4
Alexandru Stanciu <alexandru.stanciu@gmail.com>
2014-08-09 23:55:22 +0200

    Most followed in storage

--- storage.py
+++ storage.py
@@ -43,27 +43,35 @@ class Storage(object):
 		"Return user_id's friends list."
 		return None
 
+	def most_followed(self):
+		"""
+		Aggregate most followed users from the store.
+		Return top 5000.
+		"""
+		return None
+
 
 class RedisStorage(Storage):
 	"Redis storage "
 	
 	import redis
-	r = redis.StrictRedis(db=1)
+	r = redis.StrictRedis()
 
 	def set_user_data(self, user_id, user_data):
 		self.r.hmset('user_data:%s' % user_id, user_data)
 		super(RedisStorage, self).set_user_data(user_id, user_data)
 
 	def is_protected(self, user_id):
-		return self.r.sismember('system_protected', user_id)
+		return self.r.sismember('protected_users', user_id)
 
 	def mark_protected(self, user_id):
-		self.r.sadd('system_protected', user_id)
+		self.r.sadd('protected_users', user_id)
 		super(RedisStorage, self).mark_protected(user_id)
 
 	# Followers
 	def set_followers(self, user_id, followers):
 		"Store user_id's followers list."
+		self.r.delete('user_followers:%s' % user_id)
 		self.r.sadd('user_followers:%s' % user_id, *followers)
 		super(RedisStorage, self).set_followers(user_id, followers)
 
@@ -78,12 +86,18 @@ class RedisStorage(Storage):
 
 	def set_friends(self, user_id, friends):
 		"Store user_id's friends list."
+		self.r.delete('user_friends:%s' % user_id)
 		self.r.sadd('user_friends:%s' % user_id, *friends)
 		super(RedisStorage, self).set_friends(user_id, friends)
 
 	def get_friends(self, user_id):
 		"Return user_id's friends list."
 		return self.r.smembers('user_friends:%s' % user_id)
+
+	def most_followed(self):
+		keys = r.keys('user_friends:*')
+		r.zunionstore('most_followed', keys)
+		return r.zrevrange('most_followed', 0, 4999, withscores=True)
 
 
 class FileStorage(Storage):
